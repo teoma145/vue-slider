12440095d753b64b316fc2074934a4c8010bab17
teoma145 <matteodark145@yahoo.it>
2023-10-30 15:33:30 +0100

    slideshtml

--- js/script.js
+++ js/script.js
@@ -27,7 +27,7 @@ Vue.createApp({
                         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
                     }
                 ],
-                currentIndex: 0 
+                currentIndex: 1 
         };
     },
     methods:{
